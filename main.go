c4829f005f11f74bc8ea0fb0dfe407e79bd16b06
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2026-07-01 00:44:39 -0400

    make it so you can upload images and view them

---
+++ main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const port = "5465"
+const maxMemoryBytesForParsingImage int64 = 1 << 20
+const maxUploadRequestSize int64 = 1 << 20
+
+type App struct {
+	DB *sql.DB
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "graffiti.sqlite3")
+	if err != nil {
+		log.Fatal("failed to connect to database")
+	}
+	fmt.Println("connected to database")
+	defer db.Close()
+
+	app := App{DB: db}
+
+	http.HandleFunc("GET /{$}", app.handleGetHome)
+	http.HandleFunc("GET /image/{id}", app.handleGetImage)
+	http.HandleFunc("POST /image/create", app.handlePostImage)
+
+	fmt.Printf("listening for http requests on http://localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func (app App) handleGetHome(w http.ResponseWriter, r *http.Request) {
+	bytes, err := os.ReadFile("home.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write(bytes)
+}
+
+func (app App) handleGetImage(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	if strings.TrimSpace(id) == "" {
+		http.Error(w, "Must provide nonempty image ID.", http.StatusBadRequest)
+	}
+	row := app.DB.QueryRow("SELECT filename, data FROM images WHERE id = ?", id)
+
+	filename := ""
+	blob := []byte{}
+	err := row.Scan(&filename, &blob)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.NotFound(w, r)
+			return
+		}
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// provide a filename for the Save As prompt to use if the user happens to use it
+	// (hopefully will also convince the Images spigot plugin to display this filename in chat)
+	w.Header().Add("Content-Disposition", `inline; filename*=UTF-8''`+filename)
+
+	w.Write(blob)
+}
+
+func (app App) handlePostImage(w http.ResponseWriter, r *http.Request) {
+	// limit size of incoming requests
+	// (turn the body into a limitreader that will eof when the max size is reached while reading)
+	r.Body = http.MaxBytesReader(w, r.Body, maxUploadRequestSize)
+
+	// try to parse the form data (which presumably reads from r.Body)
+	if err := r.ParseMultipartForm(maxMemoryBytesForParsingImage); err != nil {
+
+		// if an error occurs while reading, check if the error was caused by the limitreader
+		// this is done by checking if err is a pointer to a MaxBytesError (https://github.com/golang/go/issues/30715)
+		if maxBytesErr, ok := err.(*http.MaxBytesError); ok {
+			http.Error(
+				w,
+				fmt.Sprintf("Uploaded files must be no more than %d bytes in filesize.", maxBytesErr.Limit),
+				http.StatusRequestEntityTooLarge)
+			return
+		}
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer r.MultipartForm.RemoveAll()
+
+	uploadedFile, uploadedFileHeader, err := r.FormFile("uploaded-image")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer uploadedFile.Close()
+
+	blob, err := io.ReadAll(uploadedFile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	now := time.Now()
+	expiry := now.Add(time.Hour * 24 * 7)
+
+	result, err := app.DB.Exec(
+		"INSERT INTO images (data, filename, created_at, expires_at) VALUES (?, ?, ?, ?)",
+		blob,
+		uploadedFileHeader.Filename,
+		now.Unix(),
+		expiry.Unix())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "The image was uploaded at %v and can be viewed at /image/%v. It will expire at %v.",
+		now, id, expiry)
+}

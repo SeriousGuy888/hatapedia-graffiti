95efb80a00e05a25597bbbaca799ea42a707f3dd
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2026-07-01 20:21:53 -0400

    made a canvas you can draw on

---
+++ web/static/graffiti_canvas.js
@@ -0,0 +1,108 @@
+import { MapColour, MapColourRGBA } from "./colours.js"
+
+const canvasWidth = 128
+const canvasHeight = 128
+
+/** @type HTMLCanvasElement */
+const canvas = document.getElementById("graffiti-canvas")
+const ctx = canvas.getContext("2d")
+
+/**
+ * An array of the pixels stored in row major order.
+ * Each pixel is one uint8, an index into the colour table.
+ */
+const pixelData = new Uint8Array(canvasWidth * canvasHeight)
+
+/**
+ * This object represents the image data drawn to the canvas.
+ * It should be kept in sync with `pixelData`.
+ */
+const imageData = new ImageData(canvasWidth, canvasHeight)
+
+/** Sets all pixels to be transparent. */
+function clearCanvas() {
+	pixelData.fill(MapColour.NONE)
+	imageData.data.fill(0)
+}
+
+/**
+ * @param x {number}
+ * @param y {number}
+ * @param mapColour {number} An index into the map colour palette.
+ */
+function setPixel(x, y, mapColour) {
+	pixelData[128 * y + x] = MapColour.WATER
+	imageData.data.set(
+		MapColourRGBA[MapColour.WATER],
+		canvasWidth * (y * 4) + x * 4,
+	)
+}
+
+/**
+ * @param xCenter {number}
+ * @param yCenter {number}
+ * @param radius {number}
+ * @param mapColour {number} An index into the map colour palette.
+ */
+function drawCircle(xCenter, yCenter, radius, mapColour) {
+	if (radius < 0) return
+
+	// loop through all pixels in a (2r+1)×(2r+1) square around (xCenter,yCenter)
+	// test if that pixel is _strictly_ within a radius of r of the center
+	//
+	// (
+	// strictly < radius (rather than <= radius)
+	// for the sake of removing that one extra pixel poking out along
+	// the orthogonal directions from the center on the edge that looks a bit ugly
+	// )
+	const radiusSquared = radius * radius
+	for (let dy = -radius; dy <= radius; dy++) {
+		for (let dx = -radius; dx <= radius; dx++) {
+			if (dx * dx + dy * dy < radiusSquared) {
+				const x = xCenter + dx
+				const y = yCenter + dy
+				if (x >= 0 && x < canvasWidth && y >= 0 && y < canvasHeight) {
+					setPixel(x, y, mapColour)
+				}
+			}
+		}
+	}
+}
+
+function initEvents() {
+	document
+		.getElementById("clear-canvas-button")
+		.addEventListener("mouseup", clearCanvas)
+}
+
+function initCanvas() {
+	if (!ctx) {
+		console.error("Cannot get 2d context for canvas.")
+		return
+	}
+
+	canvas.addEventListener("mousemove", (event) => {
+		if (!(event.buttons & 1)) {
+			// if not leftclicking
+			return
+		}
+
+		const boundingRect = canvas.getBoundingClientRect()
+		const scaleX = canvas.width / boundingRect.width
+		const scaleY = canvas.height / boundingRect.height
+		const x = Math.floor((event.clientX - boundingRect.left) * scaleX)
+		const y = Math.floor((event.clientY - boundingRect.top) * scaleY)
+
+		drawCircle(x, y, 4, MapColour.WATER)
+	})
+
+	requestAnimationFrame(draw)
+}
+
+function draw() {
+	ctx.putImageData(imageData, 0, 0)
+	requestAnimationFrame(draw)
+}
+
+initEvents()
+initCanvas()
